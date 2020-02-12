6f2fe31dd7eee5c968dea677161fd5ecb1940317
Vinicius Isola <viniciusisola@gmail.com>
2020-02-12 06:38:48 -0500

    Remove nginx ingress controller

--- terraform/shared/nginx_ingress_controller/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "external_ip_address" {
-  description = "IP address of the external load balancer for the nginx-ingress."
-  value       = kubernetes_service.nginx_ingress.load_balancer_ingress[0].ip
-}
