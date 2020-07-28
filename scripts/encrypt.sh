4b751bb6fd1d45bbd52d590930dd21df0003981f
Vinicius Isola <viniciusisola@gmail.com>
2020-07-27 23:06:29 -0400

    Remove unused scripts, re-encrypt secrets

--- scripts/encrypt.sh
+++ scripts/encrypt.sh
@@ -8,11 +8,7 @@ ENCRYPTED_FILE=encrypted.taz.gz.gpg
 
 rm -f $FILE_TO_ENCRYPT $ENCRYPTED_FILE
 tar -czvf $FILE_TO_ENCRYPT \
-  environments/dev/cert.pem \
-  environments/dev/privkey.pem \
-  environments/dev/credentials.json \
-  environments/dev/01-base/terraform.tfvars \
-  environments/dev/03-applications/terraform.tfvars
+  credentials.json
 
 gpg2 --symmetric --batch --yes --passphrase "$PASSWORD" $FILE_TO_ENCRYPT
 md5 $FILE_TO_ENCRYPT
