7e3e7d5923457dc3a0ddc6e1d3dd4428dcba7aa3
Vinicius Isola <viniciusisola@gmail.com>
2020-09-09 00:14:36 -0400

    Add photos backend

--- infrastructure/environments/dev/c_deploy/__init__.py
+++ infrastructure/environments/dev/c_deploy/__init__.py
@@ -9,6 +9,8 @@ def create(*, main_server_name, mysql_appuser, mysql_appuser_password):
     mysql_appuser_password = mysql_appuser_password,
   )
 
+  deploy_photos(main_server=main_server)
+
 def deploy_my_finances(*, main_server, mysql_appuser, mysql_appuser_password):
   # Get code
   main_server.run_command("cd /opt/apps; sudo rm -Rf my_finances; sudo git clone https://github.com/VinnieApps/my_finances.git; sudo chown -R $(whoami):$(whoami) my_finances")
@@ -26,3 +28,17 @@ def deploy_my_finances(*, main_server, mysql_appuser, mysql_appuser_password):
 
   # Fix ownership and restart application
   main_server.run_command("sudo chown -R appuser:appuser /opt/apps/my_finances; sudo supervisorctl restart my_finances")
+
+def deploy_photos(*, main_server):
+  # Download JARs
+  command = "mkdir -p /opt/apps/photos; cd /opt/apps/photos; sudo chown -R $(whoami):$(whoami) /opt/apps/photos;"
+  command = command + "latest_release_json=$(curl -L https://api.github.com/repos/VinnieApps/photos/releases/latest);"
+  command = command + "photos_jobs_url=$(echo $latest_release_json | jq -r '.assets[0].browser_download_url'); curl -L -o photos-jobs.jar $photos_jobs_url;"
+  command = command + "photos_service_url=$(echo $latest_release_json | jq -r '.assets[1].browser_download_url'); curl -L -o photos-service.jar $photos_service_url;"
+  main_server.run_command(command)
+
+  # Copy configs
+  main_server.run_command("sudo cp /opt/apps/configs/photos/application.yml /opt/apps/photos/application.yml")
+
+  # Fix ownership and restart application
+  main_server.run_command("sudo chown -R appuser:appuser /opt/apps/photos; sudo supervisorctl restart photos-service photos-jobs")
